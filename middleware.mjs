6a195fe70f7fe6a6012fb15160cc6feaba033875
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-09 19:35:10 +0530

    Implement authentication middleware to validate JWT tokens in requests

--- middleware.mjs
+++ middleware.mjs
@@ -1,13 +1,34 @@
+import jwt from 'jsonwebtoken';
+
 const globalMiddleware = (req, res, next) => {
-  req.name = "apple"
-  console.log("global middleware apple")
+  req.name = 'apple';
+  console.log('global middleware apple');
   next();
-}
+};
 
 const authMiddleware = (req, res, next) => {
-  console.log("check here for login")
-  res.json({ error: "you are not logged in" })
-  // next()
-}
+  // check for token is available or not
+  if (!req.headers.authorization) {
+    res.statusCode = 400;
+    return res.json({ error: 'token is not sent' });
+  }
 
-export { globalMiddleware, authMiddleware }
+  // check for Barer is present or not
+  const barerToken = req.headers.authorization.split(' ');
+  const token = barerToken[1];
+  if (!token) {
+    res.statusCode = 400;
+    return res.json({ error: 'token not valid' });
+  }
+
+  // validate token
+  try {
+    jwt.verify(token, 'kuguogyfyfvhgyvhuofkygvkiulju');
+  } catch (e) {
+    res.statusCode = 400;
+    return res.json({ error: e.message });
+  }
+  next();
+};
+
+export { globalMiddleware, authMiddleware };
