ee8070bbe28b15d355bc8e7bf452038243e07341
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-15 15:36:14 +0530

    Implement password hashing with bcrypt in user registration and login

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -1,5 +1,7 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import jwt from 'jsonwebtoken';
+import prisma from '../db.mjs';
+import bcrypt from 'bcrypt';
 
 const registerController = async (req, res, next) => {
   // input check
@@ -9,35 +11,17 @@ const registerController = async (req, res, next) => {
     // throw new Error(JSON.stringify({ error: "input is not valid" }))
   }
 
-  // db file read
-  const fileDataStr = await readFile('./db.json', {
-    encoding: 'utf-8',
+  // hash password of user
+  const newHashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  // add user in DB
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      password: newHashedPassword,
+    },
   });
-  // parser string to json object
-  const fileData = JSON.parse(fileDataStr);
-
-  // user data object
-  const userData = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  // check if user already registered
-  if (
-    fileData.users.filter((e) => {
-      return e.email === userData.email;
-    }).length > 0
-  ) {
-    res.statusCode = 400;
-    return res.json({ error: 'user already registered' });
-  }
-
-  // user data added to json data
-  fileData.users.push(userData);
-
-  // db json update
-  await writeFile('./db.json', JSON.stringify(fileData));
 
   // send response
   res.json({ message: 'register successful' });
@@ -50,25 +34,27 @@ const loginController = async (req, res, next) => {
     return res.json({ error: 'input is not valid' });
   }
 
-  // read db in string
-  const fileDataStr = await readFile('./db.json', { encoding: 'utf-8' });
-  // convert db data in json
-  const dbData = JSON.parse(fileDataStr);
-
-  // find user in db
-  const user = dbData.users.filter((e) => {
-    return e.email === req.body.email;
-  })[0];
+  // find user in DB
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (!user) {
+    res.statusCode = 404;
+    return res.json({ error: 'user DNE' });
+  }
 
   // match password
-  if (user.password !== req.body.password) {
+  const isOk = await bcrypt.compare(req.body.password, user.password);
+  if (!isOk) {
     res.statusCode = 400;
     return res.json({ error: 'password is wrong' });
   }
 
   const token = jwt.sign(
     { name: user.name, email: user.email },
-    'kuguogyfyfvhgyvhuofkygvkiulju',
+    process.env.TOKEN_SECRET,
     { expiresIn: '1h' }
   );
 
