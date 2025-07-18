4b4176b697ae5c85be9ce3441210207763e7f4dd
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-19 04:26:20 +0530

    Add forgot password functionality with email sending capability

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
-
-import { registerController, loginController } from './controllers/user.mjs';
+import {
+  registerController,
+  loginController,
+  forgotPasswordController,
+} from './controllers/user.mjs';
 import { prizeController } from './controllers/prize.mjs';
 import { globalMiddleware, authMiddleware } from './middleware.mjs';
 import { errorController, undefinedRouteHandler } from './error.mjs';
 const server = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.use(express.json());
 
@@ -13,6 +17,7 @@ server.use(globalMiddleware);
 
 server.post('/register', registerController);
 server.post('/login', loginController);
+server.post('/forgot_password', forgotPasswordController);
 server.get('/prize', authMiddleware, prizeController);
 
 // Catch-all route
