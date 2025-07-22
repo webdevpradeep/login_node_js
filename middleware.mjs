a298623fcbcb32d5de2dcddb48753592ebf7d11b
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-22 16:19:57 +0530

    Add user role management and implement permission middleware for auth…

--- middleware.mjs
+++ middleware.mjs
@@ -28,7 +28,25 @@ const authMiddleware = (req, res, next) => {
     res.statusCode = 400;
     return res.json({ error: e.message });
   }
+
+  req.user = jwt.decode(token);
+
   next();
 };
 
-export { globalMiddleware, authMiddleware };
+const permissionMiddleware = (...roles) => {
+  return async (req, res, next) => {
+    if (!req.user || !req.user.role) {
+      res.statusCode = 500;
+      return res.json({ message: 'authentication not added correctly' });
+    }
+    const role = roles.filter((e) => e === req.user.role);
+    if (role.length == 0) {
+      res.statusCode = 401;
+      return res.json({ message: 'you are not authorized!!!' });
+    }
+    next();
+  };
+};
+
+export { globalMiddleware, authMiddleware, permissionMiddleware };
