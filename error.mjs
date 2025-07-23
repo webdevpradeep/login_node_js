a2a6b7cf5ced4c9e40062954b133429275167ae5
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-23 17:53:52 +0530

    demo catchController function for error handling

--- error.mjs
+++ error.mjs
@@ -1,10 +1,35 @@
 const errorController = (err, req, res, next) => {
-  console.log(err)
-  res.json({ error: "something is not ok" })
-}
+  if (err.isManual) {
+    res.statusCode = err.statusCode;
+    res.json({ error: err.message });
+  } else {
+    console.log(err.stack);
+    res.statusCode = 500;
+    res.json({ error: 'something is not ok' });
+  }
+};
+
+// const catchController = (fn) => {
+//   return async (req, res, next) => {
+//     try {
+//       await fn()
+//     } catch (e) {
+//       res.statusCode = e.statusCode
+//       res.json({ error: e.message })
+//     }
+//   }
+// }
 
 const undefinedRouteHandler = (req, res) => {
   res.json({ message: 'wrong route' });
+};
+
+class ServerError extends Error {
+  constructor(statusCode, errorMessage) {
+    super(errorMessage);
+    this.statusCode = statusCode;
+    this.isManual = true;
+  }
 }
 
-export { errorController, undefinedRouteHandler }
+export { errorController, undefinedRouteHandler, ServerError };
