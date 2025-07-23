9160662e5e7372bd17dde4e0717ce4536f352629
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-07-23 17:54:35 +0530

    Refactor error handling to use ServerError class for consistent error…

--- middleware.mjs
+++ middleware.mjs
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { ServerError } from './error.mjs';
 
 const globalMiddleware = (req, res, next) => {
   req.name = 'apple';
@@ -9,24 +10,21 @@ const globalMiddleware = (req, res, next) => {
 const authMiddleware = (req, res, next) => {
   // check for token is available or not
   if (!req.headers.authorization) {
-    res.statusCode = 400;
-    return res.json({ error: 'token is not sent' });
+    throw new ServerError(400, 'token is not sent');
   }
 
   // check for Barer is present or not
   const barerToken = req.headers.authorization.split(' ');
   const token = barerToken[1];
   if (!token) {
-    res.statusCode = 400;
-    return res.json({ error: 'token not valid' });
+    throw new ServerError(400, 'token not valid');
   }
 
   // validate token
   try {
     jwt.verify(token, process.env.TOKEN_SECRET);
   } catch (e) {
-    res.statusCode = 400;
-    return res.json({ error: e.message });
+    throw new ServerError(400, e.message);
   }
 
   req.user = jwt.decode(token);
@@ -37,13 +35,11 @@ const authMiddleware = (req, res, next) => {
 const permissionMiddleware = (...roles) => {
   return async (req, res, next) => {
     if (!req.user || !req.user.role) {
-      res.statusCode = 500;
-      return res.json({ message: 'authentication not added correctly' });
+      throw new ServerError(500, 'authentication not added correctly');
     }
     const role = roles.filter((e) => e === req.user.role);
     if (role.length == 0) {
-      res.statusCode = 401;
-      return res.json({ message: 'you are not authorized!!!' });
+      throw new ServerError(401, 'you are not authorized!!!');
     }
     next();
   };
